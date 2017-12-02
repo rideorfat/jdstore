b8bfaa8e527bce1b3b1a8302bcab4550ce57d1bb
rideorfat <rideorfat@gmail.com>
2017-12-03 01:54:53 +0800

    cannot buy when quatity 0

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -14,8 +14,12 @@ class CartItemsController < ApplicationController
   def update
      @cart = current_cart
      @cart_item = @cart.cart_items.find_by(product_id: params[:id])
-     @cart_item.update(cart_item_params)
-
+     if @cart_item.product.quantity >= cart_item_params[:quantity].to_i
+       @cart_item.update(cart_item_params)
+       flash[:notice] = "成功變更數量"
+     else
+       flash[:warning] = "庫存數量不足"
+     end
      redirect_to carts_path
    end
 
