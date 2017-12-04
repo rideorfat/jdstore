398e705b5c506ecb446a5a958f3d0988720f6d6e
rideorfat <rideorfat@gmail.com>
2017-12-04 13:20:35 +0800

    add order controller

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,25 @@
 class Order < ApplicationRecord
+  before_action :authenticate_user!, only: [:create]
+  belongs_to :user
+  validates :billing_name, presence: true
+  validates :billing_address, presence: true
+  validates :shipping_name, presence: true
+  validates :shipping_address, presence: true
+
+  def create
+    @order = Order.new(order_params)
+    @order.user = current_user
+    @order.total = current_cart.total_price
+
+    if @order.save
+      redirect_to order_path(@order)
+    else
+      reder 'carts/checkout'
+    end
+
+    private
+
+    def order_params
+      params.require(:order).permit(:billing_name, :billing_address, :shipping_name, :shipping_address)
+    end
 end
