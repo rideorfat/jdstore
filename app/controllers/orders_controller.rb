9de16cf6073d26e7293cffdf1c423a531ae19ce2
rideorfat <rideorfat@gmail.com>
2017-12-06 22:18:06 +0800

    add notify mail f

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:create]
 
-    
+
 
       def create
         @order = Order.new(order_params)
@@ -44,6 +44,13 @@ class OrdersController < ApplicationController
         @order.make_payment!
 
         redirect_to order_path(@order.token), notice: "使用微信成功完成付款"
+      end
+
+      def apply_to_cancel
+          @order = Order.find_by_token(params[:id])
+          OrderMailer.apply_cancel(@order).deliver!
+          flash[:notice] = "已提交申请"
+          redirect_to :back
       end
 
     private
